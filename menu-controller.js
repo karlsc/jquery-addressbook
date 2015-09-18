af727062f3e9afabe75cee4e405c7c4e7ff9007d
Willyboy2101 <guillaumeboutin2101@gmail.com>
2015-09-18 17:53:52 -0400

    Added and completed search feature in menu-controller.js.

--- menu-controller.js
+++ menu-controller.js
@@ -1,8 +1,10 @@
 var inquirer = require("inquirer");
 var adressList = require('./adress-list.js');
 var displayTable = require('./display-table.js');
+var prompt = require('prompt');
 
 function mainMenu() {
+
   inquirer.prompt([
     {
       type: "list",
@@ -14,27 +16,43 @@ function mainMenu() {
         "Search",
         "Quit"
       ]
-    } ], function( answers ) {
+    } ], function( answer ) {
 
-      if (answers.action === "Add" || answers.action === "Search") {
+      if (answer.action === "Add") {
         mainMenu();
       }
-      else if (answers.action === "Display all") {
-        // adressList.forEach( function(entry) {
-        //   console.log(entry["First Name"]+" "+entry["Last Name"]);
-        // })
-        selectEntry(adressList);
+      else if (answer.action === "Search") {
+
+        searchPrompt();
+
+      }
+      else if (answer.action === "Display all") {
+        selectEntry(adressList, 0);
       }
       else { console.log("Good bye!"); }
   });
 };
 
-function selectEntry(entries) {
+function searchPrompt() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'query',
+      message: 'Search for: ',
+    }
+  ], function(answer) {
+    findMatch(answer.query);
+  });
+}
+
+function selectEntry(entries, search) {
 
   var entryChoices = [];
   entries.forEach( function(entry) {
     entryChoices.push(entry["First Name"]+" "+entry["Last Name"]);
-  })
+  });
+  if (search === 1) { entryChoices.push("Make another search"); }
+  entryChoices.push("Back to main menu");
   inquirer.prompt([
     {
       type: "list",
@@ -43,10 +61,45 @@ function selectEntry(entries) {
       choices: entryChoices
     }
   ], function(answer) {
-    var index = entryChoices.indexOf(answer.action);
-    var idNumber = adressList[index].id;
-    displayTable(idNumber);
+
+    if (answer.action === "Back to main menu") {
+      mainMenu();
+    }
+    else if (answer.action === "Make another search") {
+      searchPrompt();
+    }
+    else {
+      var index = entryChoices.indexOf(answer.action);
+      var idNumber = entries[index].id;
+      displayTable(idNumber);
+    }
+
   });
+}
+
+function findMatch(queryStr) {
+  var re = new RegExp(queryStr, "i");
+  var entries = [];
+  adressList.forEach( function(entry) {
+    if (re.test(entry["First Name"]+" "+entry["Last Name"])) {
+      entries.push(entry);
+    }
+    else {
+      for (var key in entry["Emails"]) {
+        if (re.test(entry["Emails"][key])) {
+          entries.push(entry);
+          break;
+        }
+      }
+    }
+  });
+  if ( entries.length > 0) {
+    selectEntry(entries, 1);
+  }
+  else {
+    console.log("No matches found.");
+    mainMenu();
+  }
 }
 
 mainMenu();
